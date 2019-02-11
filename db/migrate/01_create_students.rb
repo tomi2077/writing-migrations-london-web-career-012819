e2e97ae914018cc048707957407949428c8aa805
Tominiyi Ajayi <tominiyimunito@192.168.1.155>
2019-02-11 00:12:55 +0000

    Done

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,16 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
+  def change
+    create_table :students do |t|
+      t.string :name
+    end
+  end
+end
+
+
+class CreateStudents < ActiveRecord::Migration[5.1]
+  def change
+    create_table :students do |t|
+      t.string :name
+      end
+    end
 end
